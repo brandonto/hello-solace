81b2f645c1b50a08709a0a232556ae2e0e2ddb7f
Brandon To <brandon_to@hotmail.com>
2016-11-02 18:52:02 -0400

    Changed web client.

---
+++ web-client/js/script.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+
+    function changeBackgroundColor(color) {
+        document.body.style.backgroundColor = color;
+    }
+
+    //changeBackgroundColor('#cbcbcb');
+    changeBackgroundColor('#000000');
+
+    var socket = new WebSocket('ws://127.0.0.1:5001');
+
+    socket.onopen = function() {
+        console.log('connection opened');
+    }
+
+    socket.onclose = function() {
+        console.log('connection closed');
+    }
+
+    socket.onmessage = function(event) {
+        console.log('message received');
+    }
+}
