4925ebd00e4c5aabf1191e1540ab016e87f25c33
Shalin <backupshalin@gmail.com>
2016-11-02 19:42:15 -0500

    tillsessionconn

--- web-client/js/script.js
+++ web-client/js/script.js
@@ -1,4 +1,6 @@
 window.onload = function() {
+var mySession = null;
+var mySessionProperties = null;
 
     function changeBackgroundColor(color) {
         document.body.style.backgroundColor = color;
@@ -7,17 +9,48 @@ window.onload = function() {
     //changeBackgroundColor('#cbcbcb');
     changeBackgroundColor('#000000');
 
-    var socket = new WebSocket('ws://127.0.0.1:5001');
+            mySessionProperties = new solace.SessionProperties();
 
-    socket.onopen = function() {
-        console.log('connection opened');
-    }
 
-    socket.onclose = function() {
-        console.log('connection closed');
-    }
+            mySessionProperties.connectTimeoutInMsecs = 25000;
+            mySessionProperties.transportDowngradeTimeoutInMsecs = 5000;
 
-    socket.onmessage = function(event) {
-        console.log('message received');
-    }
+            //mySessionProperties.readTimeoutInMsecs = OPERATION_TIMEOUT;
+            mySessionProperties.keepAliveIntervalsLimit = 10;
+            mySessionProperties.userName = "default";
+            mySessionProperties.vpnName = "";
+            mySessionProperties.password = "default";
+            mySessionProperties.url = "http://69.20.234.126:8134";
+            //mySessionProperties.reapplySubscriptions = autoReconnect;
+            mySessionProperties.keepAliveIntervalInMsecs = 3000;
+
+            
+            mySession = solace.SolclientFactory.createSession(mySessionProperties,
+                    new solace.MessageRxCBInfo(function(session, message) {
+                            messageEventCb(session, message);
+                    }, this),
+                    new solace.SessionEventCBInfo(function(session, event) {
+                        sessionEventCb(session, event);
+                    }, this));
+					
+			mySession.connect();
+			
+	    /**
+     * Direct message receive callback
+     * @param session
+     * @param message
+     */
+    function mesageEventCb (session, message) {
+        console.log("msg event cb");
+      
+    };
+	/**
+     * Session event callback
+     * @param session
+     * @param event
+     */
+    function sessionEventCb (session, event) {
+		
+      console.log("session event cb");
+    };
 }
