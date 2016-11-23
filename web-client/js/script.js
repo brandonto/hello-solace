0ae09197e61fd9de2770ce307674f04b6f55fa32
Brandon To <brandon_to@hotmail.com>
2016-11-23 18:07:22 -0500

    Fixed spacing on javascript file.

--- web-client/js/script.js
+++ web-client/js/script.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+
 var mySession = null;
 var mySessionProperties = null;
 
@@ -9,81 +10,78 @@ var mySessionProperties = null;
     //changeBackgroundColor('#cbcbcb');
     changeBackgroundColor('#000000');
 
-            mySessionProperties = new solace.SessionProperties();
+    mySessionProperties = new solace.SessionProperties();
 
+    mySessionProperties.connectTimeoutInMsecs = 25000;
+    mySessionProperties.transportDowngradeTimeoutInMsecs = 5000;
 
-            mySessionProperties.connectTimeoutInMsecs = 25000;
-            mySessionProperties.transportDowngradeTimeoutInMsecs = 5000;
+    //mySessionProperties.readTimeoutInMsecs = OPERATION_TIMEOUT;
+    mySessionProperties.keepAliveIntervalsLimit = 10;
+    mySessionProperties.userName = "default";
+    mySessionProperties.vpnName = "";
+    mySessionProperties.password = "default";
+    mySessionProperties.url = "http://69.20.234.126:8134";
+    //mySessionProperties.reapplySubscriptions = autoReconnect;
+    mySessionProperties.keepAliveIntervalInMsecs = 3000;
+    var topic = solace.SolclientFactory.createTopic("brandon");
 
-            //mySessionProperties.readTimeoutInMsecs = OPERATION_TIMEOUT;
-            mySessionProperties.keepAliveIntervalsLimit = 10;
-            mySessionProperties.userName = "default";
-            mySessionProperties.vpnName = "";
-            mySessionProperties.password = "default";
-            mySessionProperties.url = "http://69.20.234.126:8134";
-            //mySessionProperties.reapplySubscriptions = autoReconnect;
-            mySessionProperties.keepAliveIntervalInMsecs = 3000;
-var topic = solace.SolclientFactory.createTopic("brandon");
-            
-            mySession = solace.SolclientFactory.createSession(mySessionProperties,
-                    new solace.MessageRxCBInfo(function(session, message) {
-                            messageEventCb(session, message);
-                    }, this),
-                    new solace.SessionEventCBInfo(function(session, event) {
-                        sessionEventCb(session, event);
-                    }, this));
-					
-			mySession.connect();
-			
-			
-			
-	    /**
+    mySession = solace.SolclientFactory.createSession(mySessionProperties,
+            new solace.MessageRxCBInfo(function(session, message) {
+                messageEventCb(session, message);
+            }, this),
+            new solace.SessionEventCBInfo(function(session, event) {
+                sessionEventCb(session, event);
+            }, this));
+
+    mySession.connect();
+
+    /**
      * Direct message receive callback
      * @param session
      * @param message
      */
     function messageEventCb (session, message) {
-		var num = Number(message.getSdtContainer().getValue());
-		console.log(num);
-		var newColor;
-		if (num < 200){
-			newColor = '#ffffff';
-		}else if (num < 300){
-			newColor = '#eeeeee';
-		}else if (num < 400){
-			newColor = '#dddddd';
-		}else if (num < 500){
-			newColor = '#cccccc';
-		}else if (num < 600){
-			newColor = '#bbbbbb';
-		}else if (num < 700){
-			newColor = '#aaaaaa';
-		}else if (num < 800){
-			newColor = '#999999';
-		}else if (num < 900){
-			newColor = '#888888';
-		}else if (num < 1000){
-			newColor = '#777777';
-		}else if (num < 1100){
-			newColor = '#666666';
-		}else if (num < 1200){
-			newColor = '#555555';
-		}else if (num < 1300){
-			newColor = '#444444';
-		}
-		changeBackgroundColor(newColor);
-		
+        var num = Number(message.getSdtContainer().getValue());
+        console.log(num);
+        var newColor;
+        if (num < 200){
+            newColor = '#ffffff';
+        }else if (num < 300){
+            newColor = '#eeeeee';
+        }else if (num < 400){
+            newColor = '#dddddd';
+        }else if (num < 500){
+            newColor = '#cccccc';
+        }else if (num < 600){
+            newColor = '#bbbbbb';
+        }else if (num < 700){
+            newColor = '#aaaaaa';
+        }else if (num < 800){
+            newColor = '#999999';
+        }else if (num < 900){
+            newColor = '#888888';
+        }else if (num < 1000){
+            newColor = '#777777';
+        }else if (num < 1100){
+            newColor = '#666666';
+        }else if (num < 1200){
+            newColor = '#555555';
+        }else if (num < 1300){
+            newColor = '#444444';
+        }
+        changeBackgroundColor(newColor);
+
     };
-	/**
+    /**
      * Session event callback
      * @param session
      * @param event
      */
     function sessionEventCb (session, event) {
-			if (event.sessionEventCode === solace.SessionEventCode.UP_NOTICE) {
-				var requestConfirmation = true;
-				mySession.subscribe(topic, requestConfirmation);
-				console.log("session event cb: UP_NOTICE");
-			}
+        if (event.sessionEventCode === solace.SessionEventCode.UP_NOTICE) {
+            var requestConfirmation = true;
+            mySession.subscribe(topic, requestConfirmation);
+            console.log("session event cb: UP_NOTICE");
+        }
     };
 }
