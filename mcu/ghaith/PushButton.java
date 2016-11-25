57ee2ab03ce82b5b1f0145fb5fca165d84a86683
Ghaith Dalla-Ali <ghaith.hadi@gmail.com>
2016-11-25 06:10:52 +0000

    working button-push to solace-msg-pub java app

--- mcu/ghaith/PushButton.java
+++ mcu/ghaith/PushButton.java
@@ -6,6 +6,8 @@ import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import com.solacesystems.jcsmp.*;
+import com.solacesystems.common.*;
 
 /**
  *
@@ -14,6 +16,53 @@ import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 public class PushButton {
 
     public static void main(String[] args) throws InterruptedException {
+        // Check command line arguments
+        if (args.length < 4) {
+            System.out.println("Usage: PushButton <msg_backbone_ip:port> <vpn> <client-username> <topic>");
+            System.exit(-1);
+        }
+        PushButton pushButton = new PushButton();
+        pushButton.run(args);
+    }
+    
+    public static class SolPub {
+        
+        public static void pub(String... args) throws JCSMPException {
+
+            System.out.println("Pub initializing...");
+
+            // Create a JCSMP Session
+            final JCSMPProperties properties = new JCSMPProperties();
+            properties.setProperty(JCSMPProperties.HOST, args[0]);      // msg-backbone ip:port
+            properties.setProperty(JCSMPProperties.VPN_NAME, args[1]);  // message-vpn
+            properties.setProperty(JCSMPProperties.USERNAME, args[2]);  // client-username (assumes no password)
+            final JCSMPSession session =  JCSMPFactory.onlyInstance().createSession(properties);
+            
+            final Topic topic = JCSMPFactory.onlyInstance().createTopic(args[3]);
+            
+            /** Anonymous inner-class for handling publishing events */
+            XMLMessageProducer prod = session.getMessageProducer(new JCSMPStreamingPublishEventHandler() {
+                public void responseReceived(String messageID) {
+                    System.out.println("Producer received response for msg: " + messageID);
+                }
+                public void handleError(String messageID, JCSMPException e, long timestamp) {
+                    System.out.printf("Producer received error for msg: %s@%s - %s%n",
+                            messageID,timestamp,e);
+                }
+            });
+            // Publish-only session is now hooked up and running!
+            
+            TextMessage msg = JCSMPFactory.onlyInstance().createMessage(TextMessage.class);
+            final String text = "Hello world!";
+            msg.setText(text);
+            System.out.printf("Connected. About to send message '%s' to topic '%s'...%n",text,topic.getName());
+            prod.send(msg,topic);
+            System.out.println("Message sent. Exiting.");
+            session.closeSession();
+        }
+    }
+
+    public static void run(String[] args) throws InterruptedException {
 
         System.out.println("Started.");
 
@@ -27,34 +76,41 @@ public class PushButton {
         // provision gpio pin #02 as an input pin with its internal pull down resistor enabled
         final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_UP);
 
+
         // create and register gpio pin listener
         myButton.addListener(new GpioPinListenerDigital() {
-                @Override
-                public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-		    System.out.println("pressed!");
-                    // when button is pressed, speed up the blink rate on LED #2
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                System.out.println("pressed!");
+                SolPub hwp = new SolPub();
+                try {
+                    hwp.pub(args);
+                    // When message is sent, blink
                     if(event.getState().isHigh()){
-                      led2.blink(200);
+                        led1.blink(1000,1000);
+                        led2.blink(1000,1000);
                     }
                     else{
-                      led2.blink(1000);
+                        led1.blink(1000,1000);
+                        led2.blink(1000,1000);
                     }
+                } catch(Exception e){
+                    System.out.println("Error publishing message!");
                 }
-            });
 
-        // continuously blink the led every 1/2 second for 15 seconds
-        led1.blink(500, 15000);
+            }
+        });
 
-        // continuously blink the led every 1 second
-        led2.blink(1000);
-
-        System.out.println("the LED will continue blinking until the program is terminated.");
+        // // continuously blink the led every 1/2 second for 15 seconds
+        // led1.blink(500, 15000);
+        
         System.out.println("PRESS <CTRL-C> TO STOP THE PROGRAM.");
 
         // keep program running until user aborts (CTRL-C)
         while(true) {
             Thread.sleep(500);
-        }
+        } 
+
 
         // stop all GPIO activity/threads
         // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
--- mcu/ghaith/README.md
+++ mcu/ghaith/README.md
@@ -1,6 +1,12 @@
-Needs the "pi4j" Java GPIO library
+Connect the board first:
+Connect the LED to pin18 of the CANAKIT pinout strip
+Connect the button to pin27 of the CANAKIT pinout strip
 
-Once installed, run the following commands inside the current directory:
+Before running the Java app:
+1- make sure the "pi4j" Java GPIO library is properly installed:
+   sudo -E curl -s get.pi4j.com | sudo -E bash
+2- All Solace JCSMP API jars from ../solace-api/lib/ are under ./lib/
 
-sudo javac -classpath .:classes:/opt/pi4j/lib/'*' BlinkGpioExample.java
-sudo java -classpath .:classes:/opt/pi4j/lib/'*' BlinkGpioExample
+Run the following commands inside the current directory:
+sudo javac -classpath .:classes:/opt/pi4j/lib/'*':./lib/'*' PushButton.java
+sudo java -classpath .:classes:/opt/pi4j/lib/'*':./lib/'*' PushButton
